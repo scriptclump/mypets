4f024b918e10ac6c5c2769a92cec55fff9ab44ff
Basant Kumar Sharma <scriptclump@gmail.com>
2020-08-20 12:43:26 +0530

    Removes the unused code

--- services/content-service/app/v1/api/contentRoutes.js
+++ services/content-service/app/v1/api/contentRoutes.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var grnController = require('../controller/contentController');
+var contentController = require('../controller/contentController');
 
 router.use(function (req, res, next) {
     next();
 });
 
 router.post('/index', function (req, res) {
-    grnController.index(req, res);
+    contentController.index(req, res);
 });
 
 module.exports = router;
