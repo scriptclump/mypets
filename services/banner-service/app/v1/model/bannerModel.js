ded1fcfec03e651f2f3b4356cdbfde35788e1426
Basant Kumar Sharma <scriptclump@gmail.com>
2020-08-20 10:32:16 +0530

    Removes the unused codes

---
+++ services/banner-service/app/v1/model/bannerModel.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const sequelize = require('../../config/sequelize');
+const moment = require('moment');
+var database = require('../../config/mysqldb');
+let db = database.DB;
+var http = require('http');
+
+
+module.exports = {
+
+
+
+
+}
